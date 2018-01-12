b1f9bc4fde360c07e0a13535eab0e9297b1709b0
Daniel Fernández Marqués <daniFM333@gmail.com>
2018-01-12 20:48:43 +0100

    Clases añadidas
    Attack casi lista

---
+++ Fighter-2D/Assets/Scripts/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack {
+
+    public int num_attacks = 3;
+
+	public enum Attacks {
+        A,
+        B,
+        C
+    }
+    //Attacks attack;
+
+    public Attacks WinsTo(Attacks attack)
+    {
+        if ((int)attack - 1 == -1)
+            return (Attacks)num_attacks - 1;
+        else
+            return attack - 1;
+    }
+
+    public Attacks LosesTo(Attacks attack)
+    {
+        if ((int)attack + 1 == num_attacks)
+            return (Attacks)0;
+        else
+            return attack + 1;
+    }
+
+}
---
+++ Fighter-2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private Attack attack;
+
+    // Use this for initialization
+    void Start () {
+
+        attack = new Attack();
+
+        Debug.Log(attack.WinsTo(Attack.Attacks.A));
+        Debug.Log(attack.WinsTo(Attack.Attacks.B));
+        Debug.Log(attack.WinsTo(Attack.Attacks.C));
+
+        Debug.Log(attack.LosesTo(Attack.Attacks.A));
+        Debug.Log(attack.LosesTo(Attack.Attacks.B));
+        Debug.Log(attack.LosesTo(Attack.Attacks.C));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
