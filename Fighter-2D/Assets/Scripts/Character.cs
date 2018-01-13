3165cb6bc07487effd2775a92ea64ba9636824fe
Unknown <alvarogibaja09@gmail.com>
2018-01-13 09:46:50 +0100

    Completado de Character.cs
    Implementacion de clases abstractas. Archivo .meta eliminado, debería generarse uno nuevo al probarlo en el proyecto.

--- Fighter-2D/Assets/Scripts/Character.cs
+++ Fighter-2D/Assets/Scripts/Character.cs
@@ -2,17 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public class Characters : MonoBehaviour
+{
 
-    public Sprite sprite;
+    // Use this for initialization
+    Player jug; 
+    AI ia; 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public virtual void Attack()
+    {
+        Debug.Log("Ataque de Character");
+
+    }
+
+
+    void Start()
+    {
+        jug = new Player();
+        ia = new AI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            jug.Attack();
+            Debug.Log("A pulsado"); 
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            ia.Attack();
+            Debug.Log("D pulsado");
+
+        }
+    }
+    
+}
+
+public class Player : Characters
+{
+    public override void Attack()
+    {
+        Debug.Log("Ataque de Player"); 
+    }
+}
+
+public class AI : Characters
+{
+    public override void Attack()
+    {
+        Debug.Log("Ataque de IA");
+    }
 }
